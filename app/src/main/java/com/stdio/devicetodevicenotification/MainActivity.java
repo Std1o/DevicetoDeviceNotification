8faf025f5f50e2c95312b2c22ce6c64c1ff96bfa
Std1o <37378410+Std1o@users.noreply.github.com>
2021-01-29 20:08:40 +0300

    OneSignal

--- app/src/main/java/com/stdio/devicetodevicenotification/MainActivity.java
+++ app/src/main/java/com/stdio/devicetodevicenotification/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String ONESIGNAL_APP_ID = "91856ead-b8e0-4a5b-8665-d2cb1ef994a5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
     }
 }
